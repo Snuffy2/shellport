f93f01f74f8f4ca5cdbfcc8acc54730f48c04fda
Snuffy2 <Snuffy2@users.noreply.github.com>
2026-05-18 18:32:17 -0400

    Address connection logging review comments

--- application/commands/connection_logging.go
+++ application/commands/connection_logging.go
@@ -5,6 +5,7 @@ package commands
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/Snuffy2/shellport/application/log"
@@ -35,19 +36,19 @@ func sshAuthMethodDebugName(method byte) string {
 func (d connectionDebugDetails) fields() string {
 	fields := make([]string, 0, 5)
 	if d.User != "" {
-		fields = append(fields, "user="+d.User)
+		fields = append(fields, "user="+strconv.Quote(d.User))
 	}
 	if d.Address != "" {
-		fields = append(fields, "address="+d.Address)
+		fields = append(fields, "address="+strconv.Quote(d.Address))
 	}
 	if d.Network != "" {
-		fields = append(fields, "network="+d.Network)
+		fields = append(fields, "network="+strconv.Quote(d.Network))
 	}
 	if d.AuthMethod != "" {
-		fields = append(fields, "auth_method="+d.AuthMethod)
+		fields = append(fields, "auth_method="+strconv.Quote(d.AuthMethod))
 	}
 	if d.PresetID != "" {
-		fields = append(fields, "preset_id="+d.PresetID)
+		fields = append(fields, "preset_id="+strconv.Quote(d.PresetID))
 	}
 
 	return strings.Join(fields, " ")
